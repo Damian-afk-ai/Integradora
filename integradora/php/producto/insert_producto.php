da0266e79a899e19ff7d17e7e81f1f52c8a300a8
Brunne-CL <2023371132@uteq.edu.mx>
2024-07-20 19:50:24 -0600

    Update insert_producto.php

--- integradora/php/producto/insert_producto.php
+++ integradora/php/producto/insert_producto.php
@@ -1,8 +1,8 @@
 <?php 
 include '../connect.php';
 
-$query_pro = "INSERT INTO PRODUCTO (nom_pro, desa_pro, desc_pro, cost_pro, prec_pro, id_cat) 
-VALUES ('$_GET[nom_pro]', '$_GET[desa_pro]', '$_GET[desc_pro]', '$_GET[cost_pro]', '$_GET[prec_pro]', '$_GET[id_cat]')";
+$query_pro = "INSERT INTO PRODUCTO (nom_pro, desa_pro, desc_pro, cost_pro, prec_pro, id_cat, est_pro) 
+VALUES ('$_GET[nom_pro]', '$_GET[desa_pro]', '$_GET[desc_pro]', '$_GET[cost_pro]', '$_GET[prec_pro]', '$_GET[id_cat]', '$_GET[est_pro]')";
 
 $query_result = mysqli_query($conn, $query_pro);
 
